baa436cfd099859d283f61d974c3c35b9958cd6c
Matthew <matthedm@uci.edu>
2022-11-26 16:43:01 -0800

    Initial Commit

---
+++ ChineseWordOfTheDay/ChineseWordOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseWordOfTheDayApp.swift
+//  ChineseWordOfTheDay
+//
+//  Created by m on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ChineseWordOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
